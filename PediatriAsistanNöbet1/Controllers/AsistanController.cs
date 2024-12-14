a1b71493dbfbddd9bc6eb719ce89fff6fb2ea135
mümine buran <mumine.buran@ogr.sakarya.edu.tr>
2024-12-14 16:41:57 +0300

    asistan tablosu eklendi

---
+++ PediatriAsistanNöbet1/Controllers/AsistanController.cs
@@ -0,0 +1,90 @@
+﻿using PediatriAsistanNöbet1.Models.DataContext;
+using PediatriAsistanNöbet1.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PediatriAsistanNöbet1.Controllers
+{
+    public class AsistanController : Controller
+    {
+        PediatriDBContext db = new PediatriDBContext(); 
+        // GET: Asistan
+        public ActionResult Index()
+        {
+            return View(db.Asistanlar.ToList());
+        }
+
+        // GET: Asistan/Details/5
+
+
+        // GET: Asistan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Asistan/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asistan/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var asistan=db.Asistanlar.Where(x => x.AsistanID == id ).SingleOrDefault();
+            return View(asistan);
+        }
+
+        // POST: Asistan/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Asistan asistan)
+        {
+            try
+            {
+                
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asistan/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Asistan/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- PediatriAsistanNöbet1/Models/Model/Asistan.cs
+++ PediatriAsistanNöbet1/Models/Model/Asistan.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
 
 namespace PediatriAsistanNöbet1.Models.Model
 {
@@ -12,14 +13,21 @@ namespace PediatriAsistanNöbet1.Models.Model
     {
         [Key]
         public int AsistanID { get; set; }
+        [DisplayName("Asistan Ad")]
         [Required, StringLength(50)]
+
         public string Ad { get; set; }
+        [DisplayName("Soyad")]
         [Required, StringLength(50)]
         public string Soyad { get; set; }
+        [DisplayName("Telefon")]
         [StringLength(20)]
         public string Telefon { get; set; }
+        [DisplayName("Email")]
+
         [StringLength(50)]
         public string Email { get; set; }
+        [DisplayName("Adres")]
         [StringLength(200)]
         public string Adres { get; set; }
     }
